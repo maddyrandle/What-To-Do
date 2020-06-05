f6db341ef67e6a0d13681d832ec19f8596960cf2
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 17:38:21 -0500

    Update Type input to a drop down with all type options

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ type, getNewActivity, updateActivityType }) => {
+
+  const updateFormState = (e) => {
+    updateActivityType(e.target.value)
+  }
+
+  const getAnotherIdea = (e) => {
+    getNewActivity();
+  }
+
   return (
     <section className="form">
-      <label className="label">Type</label>
-      <input
-        className="type"
-        placeholder="Social"
-      />
+    <label className="label">Type</label>
 
-      <button className="another">Give me another</button>
+    <select className="type" name="type" onChange={updateFormState}>
+      <option value="education">Education</option>
+      <option value="recreational">Recreational</option>
+      <option value="social">Social</option>
+      <option value="diy">DIY</option>
+      <option value="charity">Charity</option>
+      <option value="cooking">Cooking</option>
+      <option value="music">Music</option>
+      <option value="busywork">Busywork</option>
+    </select>
+
+    <button
+      className="another"
+      onClick={getAnotherIdea}
+    >
+      Give me another idea
+    </button>
 
     </section>
   )
+
 }
 
 export default Form;
