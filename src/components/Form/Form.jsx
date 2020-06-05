f57ea893798f604b0ab9a043b6d4713f172cce02
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 18:04:26 -0500

    Clean up code

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,40 +1,27 @@
 import React from 'react';
 
 const Form = ({ type, getNewActivity, updateActivityType }) => {
-
-  const updateFormState = (e) => {
-    updateActivityType(e.target.value)
-  }
-
-  const getAnotherIdea = (e) => {
-    getNewActivity();
-  }
+  const updateType = (e) => updateActivityType(e.target.value);
+  const getAnotherIdea = (e) => getNewActivity();
 
   return (
     <section className="form">
-    <label className="label">Type</label>
-
-    <select className="type" name="type" onChange={updateFormState}>
-      <option value="education">Education</option>
-      <option value="recreational">Recreational</option>
-      <option value="social">Social</option>
-      <option value="diy">DIY</option>
-      <option value="charity">Charity</option>
-      <option value="cooking">Cooking</option>
-      <option value="music">Music</option>
-      <option value="busywork">Busywork</option>
-    </select>
-
-    <button
-      className="another"
-      onClick={getAnotherIdea}
-    >
-      Give me another idea
-    </button>
-
+      <label className="label">Type</label>
+      <select className="type" name="type" onChange={updateType}>
+        <option value="education">Education</option>
+        <option value="recreational">Recreational</option>
+        <option value="social">Social</option>
+        <option value="diy">DIY</option>
+        <option value="charity">Charity</option>
+        <option value="cooking">Cooking</option>
+        <option value="music">Music</option>
+        <option value="busywork">Busywork</option>
+      </select>
+      <button className="another" onClick={getAnotherIdea}>
+        Give me another idea
+      </button>
     </section>
   )
-
 }
 
 export default Form;
