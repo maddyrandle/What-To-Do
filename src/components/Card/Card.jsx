d8ffda64a3d3497eb21d14de27bceb9edad84811
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 18:00:55 -0500

    Pass method that updates userList array as props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import Activities from '../Activities/Activities';
 import Form from '../Form/Form';
 
-const Card = ({ activity, getNewActivity, updateActivityType }) => {
+const Card = ({ activity, getNewActivity, updateActivityType, updateUserList }) => {
   return (
     <section className="card">
-      <Activities activity={ activity.activity } />
+      <Activities
+        activity={ activity.activity }
+        userList={ activity.userList }
+        updateUserList={ updateUserList }
+      />
       <Form
         type={ activity.type }
         getNewActivity={ getNewActivity }
