7fa3da725fcc159cbb4aac47fce41392c2c8f235
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 18:05:42 -0500

    Call App method to update userList when an activity is saved

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const Activities = ({ activity }) => {
+const Activities = ({ activity, userList, updateUserList }) => {
+  const saveToUserList = (e) => updateUserList();
 
-
-  
   return (
     <section className="activities">
       <p className="card-title">YOU SHOULD</p>
-
       <div className="activity-card">
         <p className="description">{activity.activity}</p>
       </div>
-
-      <button className="save-activity">Save For Later</button>
+      <button className="save-activity" onClick={saveToUserList}>
+        Save For Later
+      </button>
     </section>
   )
 }
