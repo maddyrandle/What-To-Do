1124eb4d56ea072fda4295da6284634a228f7ebc
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 22:39:14 -0500

    Change Form to a class component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Form = ({ type, getNewActivity, updateActivityType }) => {
-  const updateType = (e) => updateActivityType(e.target.value);
-  const getAnotherIdea = (e) => getNewActivity();
+class Form extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        type: 'education',
+        participants: 1,
+      }
+  }
 
-  return (
-    <section className="form">
-      <label className="type-label">Type</label>
-      <select className="type" name="type" onChange={updateType}>
-        <option value="education">Education</option>
-        <option value="recreational">Recreational</option>
-        <option value="social">Social</option>
-        <option value="diy">DIY</option>
-        <option value="charity">Charity</option>
-        <option value="cooking">Cooking</option>
-        <option value="music">Music</option>
-        <option value="busywork">Busywork</option>
-      </select>
-      <button className="another-btn" onClick={getAnotherIdea}>
-        Give me another idea
-      </button>
-    </section>
-  )
+  updateType = e => this.setState({type: e.target.value});
+  updateParticipants = e => this.setState({[e.target.name]: e.target.value})
+
+  getAnotherIdea = e => this.props.getNewActivity(
+    this.state.type,
+    this.state.participants,
+  );
+
+  render() {
+    return (
+      <section className="form">
+        <label>Type</label>
+        <select className="input" name="type" onChange={this.updateType}>
+          <option value="education">Education</option>
+          <option value="recreational">Recreational</option>
+          <option value="social">Social</option>
+          <option value="diy">DIY</option>
+          <option value="charity">Charity</option>
+          <option value="cooking">Cooking</option>
+          <option value="music">Music</option>
+          <option value="busywork">Busywork</option>
+        </select>
+
+        <label>Participants</label>
+        <input
+          className="input"
+          name="participants"
+          placeholder="1"
+          value={ this.state.participants }
+          onChange={ this.updateParticipants }
+        />
+
+        <button className="another-btn" onClick={this.getAnotherIdea}>
+          Give me another idea
+        </button>
+      </section>
+    )
+  }
 }
 
 export default Form;
