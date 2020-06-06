53d0ebe832db5efaf6eec46d52cce4044e0da135
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 21:55:45 -0500

    Relocate BrowserRotuer to App component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
 import App from './components/App/App.jsx';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
-const router = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
-
-ReactDOM.render(router, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
