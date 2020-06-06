4405c666fc251cb5c99f95712a748adeb08b0478
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 21:56:25 -0500

    Import Link, and Link to home / myList component path

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Nav = () => {
+const Nav = ({ myList, handleMyList }) => {
+
+  const handleNav = (e) => {
+    handleMyList(e.target.id);
+  }
+
   return (
-    <h1>Nav Bar</h1>
+    <nav className="nav">
+      <Link
+        to={'/'}
+        className="activities-btn"
+        onClick={ handleNav }
+      >
+        <p id="activities">Activities</p>
+      </Link>
+
+      <Link
+        to={'/mylist'}
+        className="favorites-btn"
+        onClick={ handleNav }
+      >
+        <p id="mylist">My List</p>
+      </Link>
+    </nav>
   )
 }
 
