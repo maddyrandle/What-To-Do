615c9ba0e8b73b422378bada85b0df697b693c4a
Madison Randle <madisonrandle25@gmail.com>
2020-06-06 00:58:02 -0500

    Clean props, add key to each activity, link activity website to card

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
-const MyList = ({ myList, userList, clearUserList }) => {
+const MyList = ({ userList, clearUserList }) => {
+  const clearMyList = e => clearUserList();
 
-  const clearMyList = (e) => {
-    clearUserList()
+  const displayPriceKey = price => {
+    return (
+      price !== 0 && price < 0.5
+        ? <p>$</p>
+        : price !== 0 && price <= 1 && <p>$$</p>
+    )
   }
 
+
   const activities = userList.map(activity => {
+    let price = displayPriceKey(activity.price)
+
     return (
-      <div className="mylist-card">
+      <div className="mylist-card" key={ activity.key }>
+        <p>{ activity.type.toUpperCase() }</p>
         <p>{ activity.activity }</p>
+        { activity.link && <a href={activity.link} target='_blank' rel="noopener noreferrer">Open Website</a> }
+        { price }
       </div>
     )
   })
