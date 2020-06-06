75319d0d7ee0be047e01d06b1f9ac667aa0e81cf
Madison Randle <madisonrandle25@gmail.com>
2020-06-05 21:58:30 -0500

    Import BrowserRouter, and add Route paths

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
-// import { Route } from 'react-router-dom';
-
+import { BrowserRouter, Route } from 'react-router-dom'
 import Nav from '../Nav/Nav';
 import Card from '../Card/Card';
-
+import MyList from '../MyList/MyList';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      activity: {},
-      type: 'education',
-      userList: [],
-    }
+      this.state = {
+        activity: {},
+        type: 'education',
+        userList: [],
+        myList: false
+      }
   }
 
   componentDidMount = () => {
     fetch(`http://www.boredapi.com/api/activity/?type=${this.state.type}&participants=1&minprice=0&maxprice=0.5`)
       .then(response => response.json())
-      .then(activityData => this.setState(
-        {
-          activity: activityData,
-          type: activityData.type
-        }
-      ))
+      .then(activityData => this.setState({
+        activity: activityData,
+        type: activityData.type
+      }))
       .catch(err => console.error(`There was an error: ${err}`));
   }
 
@@ -34,23 +32,46 @@ class App extends Component {
       this.setState({userList: [...this.state.userList, this.state.activity]})
   }
 
+  clearUserList = () => {
+    this.setState({userList: []})
+  }
+
+  handleMyList = (id) => {
+    if (id === 'mylist') {
+      this.setState({myList: true})
+    } else {
+      this.setState({myList: false})
+    }
+  }
+
   render() {
     return (
       <main className="app-container">
-        <Nav />
-        <Card
-          activity={this.state}
-          updateActivityType={this.updateActivityType}
-          getNewActivity={this.componentDidMount}
-          updateUserList={this.updateUserList}
-        />
+        <BrowserRouter>
+          <Nav
+            myList={this.state.myList}
+            handleMyList={this.handleMyList}
+          />
+          <Route exact path='/'render={ () =>
+            <Card
+              activity={this.state}
+              getNewActivity={this.componentDidMount}
+              updateActivityType={this.updateActivityType}
+              updateUserList={this.updateUserList}
+            />
+          } />
+
+          <Route exact path='/mylist'render={ () =>
+            <MyList
+              myList={this.state.myList}
+              userList={this.state.userList}
+              clearUserList={this.clearUserList}
+            />
+          } />
+        </BrowserRouter>
       </main>
     )
   }
 }
 
 export default App;
-
-
-// <Route exact path='/Favorites' render={ () => <Favorites /> }/>
-// <Route exact path='/loading' render={ () => <Loading /> }/>
