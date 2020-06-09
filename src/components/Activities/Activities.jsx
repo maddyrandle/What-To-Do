b4102e6b5f5077522b184953155236097be5f71d
Madison Randle <madisonrandle25@gmail.com>
2020-06-09 16:48:53 -0500

    Check Activities component PropTypes

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Activities = ({ activity, updateUserList }) => {
-  const saveToUserList = (e) => updateUserList();
+  const saveToUserList = e => updateUserList();
 
   return (
     <section data-testid="activity-card" className="activities-container">
-      <section className="flex">
+      <section className="activity-card">
         <p className="you-should">YOU SHOULD</p>
-        <div className="activity-card">
-          <p className="description">{ activity.activity || activity.error }</p>
+        <div className="description">
+          { activity.activity || activity.error }
         </div>
       </section>
+
       <section className="flex">
-        <button onClick={saveToUserList}>Save For Later</button>
+        <button onClick={saveToUserList}>
+          Save For Later
+        </button>
       </section>
     </section>
   )
 }
 
 export default Activities;
+
+Activities.propTypes = {
+  activity: PropTypes.object,
+  updateUserList: PropTypes.func
+};
