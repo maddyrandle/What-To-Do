0dd2a0b58cb8e677ddf6113eb87f1dbee892227b
Madison Randle <madisonrandle25@gmail.com>
2020-06-08 19:42:09 -0500

    Test that Nav component is rendered

---
+++ src/components/Nav/Nav.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Nav from "./Nav";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Nav", () => {
+  it("Should render a Nav component", () => {
+    const nav = (
+
+        <Nav />
+
+    );
+
+  });
+
+});
