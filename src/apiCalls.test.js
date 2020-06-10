2247ab7aa853d211201d64f12ce9009feb9fbe4a
Madison Randle <madisonrandle25@gmail.com>
2020-06-09 20:28:00 -0500

    Write, and run async test

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import App from './components/App/App';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, MemoryRouter } from "react-router-dom";
-import { render, waitFor, screen, fireEvent } from "@testing-library/react";
+import App from './components/App/App';
 import '@testing-library/jest-dom';
-
+import { render, waitFor, screen } from '@testing-library/react';
 import { getActivityData } from './apiCalls';
 jest.mock('./apiCalls.js');
 
-describe('apiCalls', () => {
-
-
-  it('when the App loads, we should see an activity idea', async () => {
-    const mockGetActivityData = {
+describe('apiCall', () => {
+  it('When the App loads, we should see an activity idea', async () => {
+    const mockActivityData = {
       accessibility: 0.1,
       activity: "Read a formal research paper on an interesting subject",
       key: "3352474",
@@ -22,18 +18,13 @@ describe('apiCalls', () => {
       type: "education"
     };
 
-    // getActivityData.mockResolvedValueOnce(mockGetActivityData);
-    //
-    // const { getByText } = render(<App />);
-    //
-    // // const idea = await waitFor(() => getByText("Read a formal research paper on an interesting subject"));
-    // //     expect(idea).toBeInTheDocument();
-    //
-    // const activityContainer = screen.getByTestId('activity-card');
-    //
-    // // const activity = await waitFor(getByText('Read a formal research paper on an interesting subject'));
-    // // expect(activity).toBeInTheDocument();
-    // expect(activityContainer).toBeInTheDocument();
+    getActivityData.mockResolvedValueOnce(mockActivityData);
 
+    const { getByText } = render(<App />);
+    const activityContainer = screen.getByTestId('activity-card');
+    const activity = await waitFor( () => getByText('Read a formal research paper on an interesting subject') );
+
+    expect(activityContainer).toBeInTheDocument();
+    expect(activity).toBeInTheDocument();
   });
 });
