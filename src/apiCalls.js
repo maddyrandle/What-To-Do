b32b5b459e250db6467e506f8599ae8880d76b42
Madison Randle <madisonrandle25@gmail.com>
2020-06-25 16:38:00 -0500

    Create .env file and store environment variable

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+const environmentVariable = process.env.REACT_APP_NOT_SECRET_CODE;
+
 export const getActivityData = async (type, price) => {
   const result = await fetch(`http://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`)
   return await result.json();
