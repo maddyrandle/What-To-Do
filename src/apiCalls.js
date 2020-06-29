0a7c397dadefe2b14dbc582993daeb7b54a43762
Madison Randle <madisonrandle25@gmail.com>
2020-06-29 05:03:40 -0500

    Fetch data with https vs http

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,7 +6,7 @@
 export const getActivityData = async (type, price) => {
   const result = await fetch('https://fe-cors-proxy.herokuapp.com', {
     headers: {
-      "Target-URL": `http://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`
+      "Target-URL": `https://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`
     }
   })
   return await result.json()
