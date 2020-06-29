68a466c4a9a94adf1a0cf4f193cbd70a6f848fbd
Madison Randle <madisonrandle25@gmail.com>
2020-06-29 04:51:53 -0500

    Add proxy

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,13 @@
-const environmentVariable = process.env.REACT_APP_NOT_SECRET_CODE;
+// export const getActivityData = async (type, price) => {
+//   const result = await fetch(`http://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`)
+//   return await result.json();
+// }
 
 export const getActivityData = async (type, price) => {
-  const result = await fetch(`http://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`)
-  return await result.json();
+  const result = await fetch('https://fe-cors-proxy.herokuapp.com', {
+    headers: {
+      "Target-URL": `http://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`
+    }
+  })
+  return await result.json()
 }
