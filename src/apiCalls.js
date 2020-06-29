52f5fda03eb5661faf4894a39f1d700fcea8f9ab
Madison Randle <madisonrandle25@gmail.com>
2020-06-29 05:31:17 -0500

    Remove comment

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,12 +2,3 @@ export const getActivityData = async (type, price) => {
   const result = await fetch(`https://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`)
   return await result.json();
 }
-
-// export const getActivityData = async (type, price) => {
-//   const result = await fetch('https://fe-cors-proxy.herokuapp.com', {
-//     headers: {
-//       "Target-URL": `https://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`
-//     }
-//   })
-//   return await result.json()
-// }
