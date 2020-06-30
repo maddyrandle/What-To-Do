9c90f8c6f924b6dd4ba5f0562ddd269c10c1f1e2
Madison Randle <madisonrandle25@gmail.com>
2020-06-30 14:51:01 -0500

    Update api with proxy

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,8 @@
 export const getActivityData = async (type, price) => {
-  const result = await fetch(`https://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`)
+  const result = await fetch('https://fe-cors-proxy.herokuapp.com', {
+    headers: {
+      "Target-URL": `https://www.boredapi.com/api/activity/?type=${ type || "education" }&minprice=0&maxprice=${ price || 0.5 }`
+    }
+  })
   return await result.json();
 }
